6e9f29baf24b7b4c3b0e5e8953048dc97713b6f5
Henrique <henriquegil123@gmail.com>
2024-10-21 09:46:55 -0300

    alteracoes

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import jsonify
+from flask import jsonify, request
 from config import app,db
 from professores.models import adicionar_professor
+from turmas.models import adicionar_turma
 
 with app.app_context():
     db.create_all()
@@ -9,4 +10,14 @@ with app.app_context():
 def add_professor():
     adicionar_professor()
     return jsonify({'message': 'Professor criado com sucesso'})
+@app.route("/turmas", methods=['POST'])
+def add_turma():
+    try:
+        turma_forms = request.json
+        if not isinstance(turma_forms['descricao'], str) and not isinstance(turma_forms['professor'], str) and not isinstance(turma_forms['ativo'], bool):
+            raise FormsError('Erro no Formulário')
+    except FormsError:
+        return jsonify({'message': 'Formulário Incorreto'})
+    return turma_forms
+
 app.run(host=app.config["HOST"], port = app.config['PORT'],debug=app.config['DEBUG'])
--- turmas/models.py
+++ turmas/models.py
@@ -5,9 +5,13 @@ class Turmas(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement= True)
     descricao = db.Column(db.String)
     professor = db.Column(db.Integer, db.ForeignKey('professores.id'), nullable=False)
-    ativo = db.Column(db.Boolean, nullable=False)
+    ativo = db.Column(db.Boolean, nullable=False, default=False)
 
     def __init__(self, descricao, professor, ativo):
         self.descricao = descricao
         self.professor = professor
         self.ativo = ativo
+
+def adicionar_turma(turma_forms):
+    db.session.add(Turmas(descricao=turma_forms['descricao'], professor=['professor'], ativo=['ativo']))
+    db.commit()
