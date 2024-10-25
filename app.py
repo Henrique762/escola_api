fde470cc0b09d4a80c89d465f2bcb939e97a6a12
Henrique Gil <henriquegil123@gmail.com>
2024-10-25 01:22:01 -0300

    adicionando o blueprint no app

--- app.py
+++ app.py
@@ -22,36 +22,4 @@ def add_turma():
     adicionar_turma(turma_forms)
     return jsonify({'message': 'Turma criada com sucesso'}), 200
 
-
-### ALUNOS ROUTES
-
-@app.route(("/alunos/"), methods=['GET'])
-def list_alunos():
-    alunos = listar_alunos()
-    return jsonify(alunos)
-
-@app.route(("/alunos/<int:id>"), methods=['GET'])
-def list_aluno(id):
-    aluno = listar_aluno(id)
-    return jsonify(aluno)
-
-@app.route(("/alunos/"), methods=['POST'])
-def add_aluno():
-    aluno_forms = request.json
-    data_nascimento = datetime.strptime(aluno_forms['data_nascimento'], "%Y-%m-%d").date()
-    aluno_forms['data_nascimento'] = data_nascimento
-    adicionar_aluno(aluno_forms, id)
-    return jsonify({'message': 'Aluno criado com sucesso'}), 200
-
-@app.route(("/alunos/"), methods=['PUT'])
-def alter_aluno():
-    aluno_forms = request.json
-    resultado = alterar_dados(aluno_forms)
-    return jsonify({'message': f'{resultado}'}), 200
-
-@app.route(("/alunos/<int:id>"), methods=['DELETE'])
-def del_alunos(id):
-    print(id)
-    resultado = deletar_alunos(id)
-    return jsonify(resultado), 200
 app.run(host=app.config["HOST"], port = app.config['PORT'],debug=app.config['DEBUG'])
